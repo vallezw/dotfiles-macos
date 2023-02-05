c5e27fd7936f7946d6f1656c2a8ed135afa9bd0c
Valentin Zwerschke <vallezw@gmail.com>
2023-02-05 14:24:14 +0100

    Remove brewfile and init conda into zsh

--- .oh-my-zsh/custom/brew.zsh
+++ .oh-my-zsh/custom/brew.zsh
@@ -1,5 +1,1 @@
 eval "$(/opt/homebrew/bin/brew shellenv)"
-if [ -f $(brew --prefix)/etc/brew-wrap ];then
-  source $(brew --prefix)/etc/brew-wrap
-fi
-export HOMEBREW_BREWFILE=~/.config/brew/Brewfile
